03a8b852328545ef40535bd07af7964b865ea69b
wanted2 <77376569+wanted2@users.noreply.github.com>
2021-03-14 11:26:58 +0700

    added a check

--- build_dev.sh
+++ build_dev.sh
@@ -4,7 +4,12 @@ if [[ -d 'www' ]]; then
     cd www;
     export GA_DEV_ID=G-FAKEID;
     echo '\ngoogle_analytics: '${GA_DEV_ID}'\n' >> _config.yml;
-    JEKYLL_ENV=development bundle exec jekyll build
+    JEKYLL_ENV=development bundle exec jekyll build;
+    export GA_DEV=$(grep -e ${GA_DEV_ID} -f _site/index.html)
+    if [ "${GA_DEV}" -eq "" ]; then 
+        echo ${GA_DEV};
+        exit 1;
+    fi
 else
     exit 1;
 fi
